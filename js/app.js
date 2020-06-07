5fbf169e9572cfa9991fe95f9a0d32ec3dbdc713
AndresParraGO <andresparragn@gmail.com>
2020-06-07 11:05:23 -0500

    Add functionality to change shoe color

--- js/app.js
+++ js/app.js
@@ -1,2 +1,21 @@
 
 console.log('Hi!')
+
+const $colors = document.querySelectorAll('#colors > div')
+const $img = document.querySelector('#img')
+
+
+function changeColor(color) {
+	const $card = document.querySelector('#card')
+	$card.className = 'card'
+	$card.classList.add(`color-${color}`)
+	$img.src = `assets/nike-${color}.png`
+}
+
+
+console.log($colors)
+
+
+$colors.forEach(color => {
+	color.addEventListener('click', e => changeColor(e.target.className))
+})
